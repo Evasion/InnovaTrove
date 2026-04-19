9bdc57b425707dbce070e39cd5459e2dc1e3c647
olamipo.osibote@sbdinc.com <latitude81uk@yahoo.com>
2026-04-18 23:58:35 -0400

    test

--- client/src/app/app.routes.server.ts
+++ client/src/app/app.routes.server.ts
@@ -1,8 +1,11 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
-  {
-    path: '**',
-    renderMode: RenderMode.Prerender
-  }
+  { path: 'dashboard/jobs/:id', renderMode: RenderMode.Server },
+  { path: 'job/:id', renderMode: RenderMode.Server },
+  { path: 'job/:id/contract', renderMode: RenderMode.Server },
+  { path: 'job/:id/checkout', renderMode: RenderMode.Server },
+  { path: 'job/:id/confirmation', renderMode: RenderMode.Server },
+  { path: 'job/:id/confirm', renderMode: RenderMode.Server },
+  { path: '**', renderMode: RenderMode.Prerender },
 ];
